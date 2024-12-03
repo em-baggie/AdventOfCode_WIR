b7443daf0bcc8d4c26b9bf0813ffda160bf553ef
Emma <156218556+em-baggie@users.noreply.github.com>
2024-12-03 10:11:45 +0000

    improved part 1 day 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -5,7 +5,7 @@ mod part_2;
 fn main() {
     // part 1 solution:
     let input1 = parse::parse_input("input.txt").unwrap();
-    println!("{}", part_1::part_1(input1));
+    println!("{}", part_2::safe_reports(input1).0.len());
 
     // part 2 solution:
     
