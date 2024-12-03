b065dcd04d3da6d2284f4282bd03754f319aa027
Emma <156218556+em-baggie@users.noreply.github.com>
2024-12-03 17:45:03 +0000

    day 2 part 2 complete

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -6,7 +6,7 @@ fn parse_input(file_path: &str) -> Result<String, std::io::Error> {
     Ok(file)
 }
 
-fn part_1(input: &str) -> usize {
+fn mul_results_part_1(input: &str) -> usize {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("Invalid regex");
     let mut total: usize = 0;
 
@@ -19,13 +19,33 @@ fn part_1(input: &str) -> usize {
     total
 }
 
+fn mul_results_part_2(input: &str) -> usize {
+    let re = Regex::new(r"(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))").expect("Invalid regex");
+    let mut total: usize = 0;
+    let mut skip = false;
+
+    re.captures_iter(input)
+        .for_each(|cap| {
+            if &cap[1] == "don't()" {
+                skip = true; 
+            } else if &cap[1] == "do()" {
+                skip = false;
+            } else if !skip {
+                let x: usize = cap[2].parse().expect("Unable to parse");
+                let y: usize = cap[3].parse().expect("Unable to parse");
+                total += x * y;
+            }
+        });
+    total
+}
+
+
 fn main() {
     let input = parse_input("input.txt").unwrap();
-
     // part 1
-    let result = part_1(&input);
+    let result = mul_results_part_1(&input);
     println!("{}", result);
-
     // part 2
-
+    let result = mul_results_part_2(&input);
+    println!("{}", result)
 }
